d915833e724cb49a8bb96ced3589c384fad9a20e
siateet <siateet@gmail.com>
2023-05-31 19:21:13 +0530

    updated!!

---
+++ app.js
@@ -0,0 +1,18 @@
+require("./config/express");
+const connection = require("./config/mongoose");
+const config = require("./config/config")
+
+
+// const db = require("./models");
+console.log(config.url);
+connection
+    .connect(config.url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }).then(() => {
+        console.log("Connected to the database!");
+    })
+    .catch(err => {
+        console.log("Cannot connect to the database!", err);
+        process.exit();
+    });
---
+++ config/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url: "mongodb+srv://ARP67:Toh15693@cluster0.fihef.mongodb.net/"
+};

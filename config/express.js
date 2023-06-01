d0af9ccd8b90d1844a6ffcd2a7d66b319903ec20
AteetPrajapati <ateetprajapati2425@gmail.com>
2023-06-01 23:14:57 +0530

    updated!!

--- config/express.js
+++ config/express.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const fs = require("node:fs");
+// const fs = require("node:fs");
 
 const app = express();
 
@@ -13,12 +13,12 @@ app.get("/", (req, res) => {
   res.json({ message: "Welcome to CRUD 1." });
 });
 
-// require("../routes/user.routes")(app);
-// require("../routes/auth.routes")(app);
+require("../routes/user.routes")(app);
+require("../routes/auth.routes")(app);
 
-for (const f of fs.readdirSync("./routes")) {
-  require(`../routes/${f}`)(app);
-}
+// for (const f of fs.readdirSync("./routes")) {
+//   require(`../routes/${f}`)(app);
+// }
 
 
 const PORT = process.env.PORT || 8080;
